9fcf1db44cf62c5e8cfbae90b219190b5f07af26
Valisher Botirov <valisherbotirov1218@gmail.com>
2023-08-26 18:26:50 +0500

    fix: test yechish

--- src/components/card/TestIndex.vue
+++ src/components/card/TestIndex.vue
@@ -1,5 +1,5 @@
 <template>
-    <pre>{{item.isSolved}}</pre>
+
     <div  class="flex justify-center items-center border w-5 h-5 p-4 cursor-pointer border-[#1F2E35] transition duration-300 rounded-[2px] hover:bg-[#264653] hover:text-white" :class="[item?.isSolved ? 'solved' :'',route.query.index == item?.questionIndex ? 'active' :'']" @click="changeRoute(item?.questionIndex)">
         {{ index }}
     </div>
