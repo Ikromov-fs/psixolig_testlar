6b428f7a0ec353771e64ab286c25c66e57ab8ec3
Ikromov-fs <nodirikromov10@gmail.com>
2023-09-07 15:10:50 +0500

    style

--- src/components/home_main/Partner.vue
+++ src/components/home_main/Partner.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="bg-[#085344]">
     <div data-aos="zoom-in-up">
-      <img
+      <!-- <img
         :src="style2"
         alt="style"
         class="absolute left-20 top-1 sx:hidden mmd:block"
@@ -10,7 +10,7 @@
         :src="style6"
         alt="style"
         class="absolute right-20 bottom-0 sx:hidden mmd:block"
-      />
+      /> -->
       <div class="container mx-auto py-10">
         <div class="flex justify-center gap-1 text-lg">
           <span class="text-[#fff]">04</span>
@@ -23,12 +23,18 @@
           <div
             v-for="item in dataTests"
             :key="item?.id"
-            class="flex flex-col items-center px-3 py-10 bg-[#00000014] sx:mt-5 mmd:mt-10 box-w rounded-md cursor-pointer"
+            class="py-2 bg-[#fff] sx:mt-5 mmd:mt-10 box-w rounded-md cursor-pointer"
           >
-            <img :src="item?.image?.url" alt="image" class="w-[50px]" />
-            <h1 class="text-[20px] text-white">
-              {{ item?.name }}
-            </h1>
+            <a :href="item?.link" class="flex flex-col items-center justify-center text-center">
+              <img
+                :src="item?.image?.url"
+                alt="image"
+                class="w-[130px] h-[100px] object-cover"
+              />
+              <h1 class="text-[20px] text-black font-medium">
+                {{ item?.name }}
+              </h1>
+            </a>
           </div>
         </div>
       </div>
@@ -37,10 +43,12 @@
 </template>
 <script setup lang="ts">
 import { ref, onMounted } from "vue";
-import logo from "../../assets/svg/logo-white.svg";
-import style2 from "@/assets/image/style5.png";
-import style6 from "@/assets/image/style6.png";
+// import logo from "../../assets/svg/logo-white.svg";
+// import style2 from "@/assets/image/style5.png";
+// import style6 from "@/assets/image/style6.png";
 import axios from "@/plugins/axios.js";
+import { useRouter } from "vue-router";
+const router = useRouter();
 const dataTests = ref();
 async function partner() {
   try {
@@ -57,7 +65,7 @@ onMounted(() => {
 <style scoped>
 .box-w {
   transition: all 0.2s linear;
-  box-shadow: 0px 0px 5px #fff;
+  box-shadow: 0px 0px 5px #333;
   transform: scale(0.98);
 }
 .box-w:hover {
